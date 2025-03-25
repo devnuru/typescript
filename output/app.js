34ec647268a40b62f6a98cb9aebef18d89c95a83
Nuruzzaman Chowdhury <devnuruzzaman@gmail.com>
2025-03-25 16:55:10 +0600

    Typescript installation

---
+++ output/app.js
@@ -0,0 +1,4 @@
+"use strict";
+// console.log('Hello, TypeScript!');
+const country = 'Bangladesh is a small country';
+console.log(country);
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+// console.log('Hello, TypeScript!');
+
+const country = 'Bangladesh is a small country';
+
+console.log(country);
